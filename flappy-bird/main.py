15fdf5bfd919158d8bfe7a450fbec4650f954f14
Marcos Sardina <marcos.sardina@gmail.com>
2023-11-06 14:42:08 +1100

    flappy bird long pipes

--- flappy-bird/main.py
+++ flappy-bird/main.py
@@ -15,8 +15,8 @@ mixer.init()
 
 background_img = pygame.image.load('assets/background.png')
 bird_img = pygame.image.load('assets/flappy bird.png')
-pipe_b_img = pygame.image.load('assets/pipe_bottom.png')
-pipe_u_img = pygame.image.load('assets/pipe_up.png')
+pipe_b_img = pygame.image.load('assets/pipe_bottom_long.png')
+pipe_u_img = pygame.image.load('assets/pipe_bottom_long.png')
 
 # Music
 
@@ -24,11 +24,13 @@ flap = pygame.mixer.Sound('music/flap.mp3')
 # Vars
 
 run = True
+clock = pygame.time.Clock()
 
 # Const
 
 WIDTH = background_img.get_width()
 HEIGHT = background_img.get_height()
+FPS = 60
 
 # Setup Screen
 
@@ -61,7 +63,7 @@ class Bird:
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         
     def keys(self, keys):
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_UP] or keys[pygame.K_SPACE]:
             if self.release:
                 flap.play()
                 self.vel = -10
@@ -69,6 +71,7 @@ class Bird:
             
         else:
             self.release = True
+            
     def check_death(self):
         
         if self.y > self.max_y:
@@ -95,6 +98,7 @@ class Pipe:
         self.x -= 10
         if self.x < 0:
             self.x = WIDTH
+            self.y = random.randint(HEIGHT - self.img.get_height(), HEIGHT - 200)
             
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
@@ -108,9 +112,9 @@ class Pipe:
         
 # Objects
 
-player = Bird(100, HEIGHT // 2, HEIGHT, bird_img, screen, )
-pipe_b = Pipe(WIDTH, HEIGHT - pipe_b_img.get_height(), pipe_b_img.get_width(), pipe_b_img.get_height(), pipe_b_img, screen)
-pipe_u = Pipe(WIDTH, 0, pipe_u_img.get_width(), pipe_u_img.get_height(), pipe_u_img, screen)
+player = Bird(100, WIDTH // 2, HEIGHT,  bird_img, screen)
+pipe_b = Pipe(WIDTH, WIDTH // 2, pipe_b_img.get_width(), pipe_b_img.get_height(), pipe_b_img, screen)
+#pipe_u = Pipe(WIDTH, pipe_b.y + (pipe_u_img.get_height() - 200), pipe_u_img.get_width(), pipe_u_img.get_height(), pipe_u_img, screen)
 
 # Game Loop
 
@@ -135,14 +139,14 @@ while run:
     screen.blit(background_img, (0, 0))
     player.draw()
     pipe_b.draw()
-    pipe_u.draw()
+    #pipe_u.draw()
     
     # Move
     
     player.move()
     pipe_b.move()
-    pipe_u.move()
-    
+    #pipe_u.move()
+    #pipe_u.y = 0 + pipe_b.y + (pipe_u_img.get_height() - 200)
     # Check for the player going out of screen
     
     if player.check_death():
@@ -153,9 +157,11 @@ while run:
     if pipe_b.collide_check(player.rect):
         run = False
     
-    if pipe_u.collide_check(player.rect):
-        run = False
+    #if pipe_u.collide_check(player.rect):
+    #    run = False
+        
     # Update
+    clock.tick(FPS )
     pygame.display.update()
     
 # Quit Game
