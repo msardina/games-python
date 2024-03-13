84020feeef19a7cc76ac2093525cb51dd5b9eece
Marcos Sardina <marcos.sardina@gmail.com>
2024-03-13 17:19:09 +1100

    add title screen image

--- horizon-powerofthesea/main.py
+++ horizon-powerofthesea/main.py
@@ -5,25 +5,29 @@ from pygame import mixer
 pygame.init()
 mixer.init()
 
+#imgs
+start_screen_img = pygame.image.load('assets/title-screen.png')
+
 #vars
 clock = pygame.time.Clock()
 
 #const
-WIDTH, HEIGHT = 1200, 800
+WIDTH, HEIGHT = start_screen_img.get_width(), start_screen_img.get_height()
 FPS = 60
 
 #font
 title_font = pygame.font.SysFont(None, 100)
 font = pygame.font.SysFont(None, 50)
 
+
+
 #setup the screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Horizon I || Power of the Sea')
 
 def start_screen():
-    print('start')
+
     start = True
-    title = title_font.render(f'Horizon I - Power of the Sea', True, (0, 0, 0))
     play = font.render(f'Press Enter to Play', True, (0, 0, 0))
 
     while start:
@@ -36,8 +40,7 @@ def start_screen():
                 quit()
                 
         #draw
-        screen.fill('sky blue')
-        screen.blit(title, ((WIDTH // 2) - (title.get_width() // 2), 100))
+        screen.blit(start_screen_img, (0, 0))
         screen.blit(play, (0, HEIGHT - play.get_height()))
         
         #move
@@ -54,7 +57,6 @@ def start_screen():
 
 
 def game():
-    print('game')
     run = True
     
     while run:
